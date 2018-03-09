d6485559b464d42c611624c1ffad6930976c35de
Kwee Lung Sin <lungsin@Klungss-MBP.lan>
2018-03-09 22:48:59 +0800

    maxclique can find which node belongs to the maxclique

--- MaxClique.cpp
+++ MaxClique.cpp
@@ -6,8 +6,8 @@
 int g[N][N], n;
 
 // MaxClique
-int dp[N], best;
-int dfs(int n, int* u, int size) {
+int dp[N], best, ans[N];
+int dfs(int n, int* u, int size, int *clique) {
   int i, j, vn, v[N];
   if (n) {
     if (size + dp[u[0]] <= best)
@@ -16,26 +16,30 @@ int dfs(int n, int* u, int size) {
       for (j = i + 1, vn = 0; j < n; ++j)
         if (g[u[i]][u[j]])
           v[vn++] = u[j];
-      if (dfs(vn, v, size + 1))
+	  clique[size] = u[i];
+      if (dfs(vn, v, size + 1, clique))
         return 1;
     }
   } else if (size > best) {
     best = size;
+	rep(k,0,size) ans[k] = clique[k];
     return 1;
   }
   return 0;
 }
 
 int maxclique() {
-  int i, j, vn, v[N];
+  int i, j, vn, v[N], arr[N];
   best = 0;
   for (dp[i = n - 1] = 0; i >= 0; --i) {
     for (vn = 0, j = i + 1; j < n; ++j)
       if (g[i][j])
         v[vn++] = j;
-    dfs(vn, v, 1);
+	arr[0] = i;
+    dfs(vn, v, 1, arr);
     dp[i] = best;
   }
+  
   return best;
 }
 
