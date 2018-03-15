7839b7b7787e91e770f0864d0025fd1407ad6154
Kwee Lung Sin <lungsin@Klungss-MacBook-Pro.local>
2018-03-15 17:54:36 +0800

    make it nicer

--- NearestNeighbourSearch.cpp
+++ NearestNeighbourSearch.cpp
@@ -6,105 +6,141 @@ const int D = 2;
 const int N = 1<<17;
 const long long INF = 2000000000000000007;
  
-struct point {
+struct Point {
 	int p[D];
-	bool operator !=(const point &a) const {
+	bool operator !=(const Point &a) const {
         for (int i = 0; i < D; i++) 
             if (p[i] != a.p[i]) 
                 return true;
 		return false;
 	}
 };
- 
-struct kd_node {
-	int axis, value;
-	point p;
-	kd_node *left, *right;
-};
- 
-struct cmp_points {
-	int axis;
-	cmp_points(){}
-	cmp_points(int x): axis(x) {}
-	bool operator () (const point &a, const point &b) const {
-		return a.p[axis]<b.p[axis];
-	}
-};
- 
-typedef kd_node* node_ptr;
- 
-int tests,n;
-point arr[N],pts[N];
-node_ptr root;
-long long ans;
- 
-long long squared_distance(point a, point b) {
-	long long ans=0;
-	for(int i=0;i<D;i++) ans+=(a.p[i]-b.p[i])*1ll*(a.p[i]-b.p[i]);
-	return ans;
+
+long long distance(Point a, Point b) {
+    long long ans=0;
+    for(int i=0;i<D;i++) ans+=(a.p[i]-b.p[i])*1ll*(a.p[i]-b.p[i]);
+    return ans;
 }
- 
-void build_tree(node_ptr &node, int from, int to, int axis) {
-	if(from>to) {
-		node=NULL;
-		return;
-	}
- 
-	node=new kd_node();
- 
-	if(from==to) {
-		node->p=arr[from];
-		node->left=NULL;
-		node->right=NULL;
-		return;
-	}
- 
-	int mid=(from+to)/2;
- 
-	nth_element(arr+from,arr+mid,arr+to+1,cmp_points(axis));
-	node->value=arr[mid].p[axis];
-	node->axis=axis;
+
+// Usage: insert all Point first, then call build()
+
+struct NearestNeighbor {
+    struct kd_node {
+        int axis, value;
+        Point p;
+        kd_node *left, *right;
+    };
     
-    int naxis = (axis == D - 1) ? 0 : axis + 1;
-	build_tree(node->left,from,mid,naxis);
-	build_tree(node->right,mid+1,to,naxis);
-}
- 
-void nearest_neighbor(node_ptr node, point q, long long &ans) {
-	if(node==NULL) return;
- 
-	if(node->left==NULL && node->right==NULL) {
-		if(q!=node->p) ans=min(ans,squared_distance(node->p,q));
-		return;
-	}
- 
-	if(q.p[node->axis]<=node->value) {
-		nearest_neighbor(node->left,q,ans);
-		if(q.p[node->axis]+sqrt(ans)>=node->value) nearest_neighbor(node->right,q,ans);
-	}
- 
-	else {
-		nearest_neighbor(node->right,q,ans);
-		if(q.p[node->axis]-sqrt(ans)<=node->value) nearest_neighbor(node->left,q,ans);
-	}
-}
- 
+    struct cmp_Points {
+        int axis;
+        cmp_Points(){}
+        cmp_Points(int x): axis(x) {}
+        bool operator () (const Point &a, const Point &b) const {
+            return a.p[axis]<b.p[axis];
+        }
+    };
+    
+    typedef kd_node* node_ptr;
+    
+    int n;
+    vector<Point> arr;
+    node_ptr root;
+    
+    void build_tree(node_ptr &node, int from, int to, int axis) {
+        if(from>to) {
+            node=NULL;
+            return;
+        }
+    
+        node=new kd_node();
+    
+        if(from==to) {
+            node->p=arr[from];
+            node->left=NULL;
+            node->right=NULL;
+            return;
+        }
+    
+        int mid=(from+to)/2;
+    
+        nth_element(arr.begin()+from,arr.begin()+mid,arr.begin()+to+1,cmp_Points(axis));
+        node->value=arr[mid].p[axis];
+        node->axis=axis;
+        
+        int naxis = (axis == D - 1) ? 0 : axis + 1;
+        build_tree(node->left,from,mid,naxis);
+        build_tree(node->right,mid+1,to,naxis);
+    }
+    
+    void nearest_neighbor(node_ptr node, Point q, Point &ans) {
+        #define dist distance(ans, q)
+        if(node==NULL) return;
+    
+        if(node->left==NULL && node->right==NULL) {
+            if(q!=node->p) {
+                if (distance(ans, q) > distance(node->p,q)){
+                    ans = node->p;
+                }
+            }
+            return;
+        }
+    
+        if(q.p[node->axis]<=node->value) {
+            nearest_neighbor(node->left, q, ans);
+            if(q.p[node->axis]+sqrt(dist)>=node->value) 
+                nearest_neighbor(node->right, q, ans);
+        }
+    
+        else {
+            nearest_neighbor(node->right, q, ans);
+            if(q.p[node->axis]-sqrt(dist)<=node->value) 
+                nearest_neighbor(node->left, q, ans);
+        }
+        #undef dist
+    }
+    
+    void insert(Point p) {
+        arr.push_back(p);
+    }
+    
+    void build() {
+        n = arr.size();
+		build_tree(root,0,n - 1,0);
+    }
+    
+    void clear() {
+        arr.clear();
+    }
+    
+    Point nearest_neighbor(Point x) {
+        Point p = x;
+        p.p[0] += 1e9;
+		nearest_neighbor(root, x, p);
+        return p;
+    }
+} KD;
+
+
+int tests, n;
+Point arr[N];
+
 int main() {
 	int i;
  
 	scanf("%d", &tests);
 	while(tests--) {
 		scanf("%d", &n);
-		for(i=1;i<=n;i++) {
+        KD.clear();
+		for(i=0;i<n;i++) {
 			scanf("%d %d", &arr[i].p[0], &arr[i].p[1]);
-			pts[i]=arr[i];
+            KD.insert(arr[i]);
 		}
+        
+        KD.build();
  
-		build_tree(root,1,n,0);
- 
-		for(i=1;i<=n;i++) {
-			ans=INF;
-			nearest_neighbor(root,pts[i],ans);
+		for(i=0;i<n;i++) {
+			Point p = KD.nearest_neighbor(arr[i]);
+            long long ans = distance(p, arr[i]);
 			printf("%lld\n", ans);
 		}
 	}
