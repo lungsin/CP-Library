4d13b021855372884d1048647aec692143f3461f
lungsin <kweelungsin@gmail.com>
2018-11-06 13:41:11 +0800

    add debug template

--- kactl-master/content/contest/template.cpp
+++ kactl-master/content/contest/template.cpp
@@ -9,6 +9,20 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+  cerr << " " << H;
+  debug_out(T...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
 int main() {
 	cin.sync_with_stdio(0); cin.tie(0);
 	cin.exceptions(cin.failbit);
